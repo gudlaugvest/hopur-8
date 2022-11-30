8850faa0394b578737f52d06cad0f9c112ddc7e0
astaduna <112621688+astaduna@users.noreply.github.com>
2022-11-30 10:53:51 +0000

    Update club_model.py

---
+++ model/club_model.py
@@ -0,0 +1,8 @@
+class Club:
+    def __init__(self, name, home_address, phone_number) -> None:
+        self.name = name
+        self.home_address = home_address
+        self.phone_number = phone_number
+
+    def __str__(self) -> str:
+        return f"Club's name: {self.name}, Home address: {self.home_address}, Phone number: {self.phone_number}"
