9cb9a9287baa22108464f74995e6c7e0897dd4c9
joejoe02 <johannagustha@gmail.com>
2022-11-30 12:02:32 +0000

    laga wrapper

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -1,9 +1,11 @@
 from data.player_data import Player_Data
+from data.team_data import Team_Data
 
 
 class Data_Wrapper:
     def __init__(self):
         self.player_data = Player_Data()
+        self.team_data = Team_Data()
 
 
     def create_player(self, player):
@@ -12,3 +14,9 @@ class Data_Wrapper:
     def get_all_players(self):
         return self.player_data.get_all_players()
 
+
+    def create_team(self):
+        return self.team_data.create_team()
+
+    def get_all_teams(self):
+        return self.team_data.get_all_teams()
--- logic/logic_wrapper.py
+++ logic/logic_wrapper.py
@@ -1,12 +1,14 @@
 
 from data.data_wrapper import Data_Wrapper
 from logic.player_logic import Player_logic
+from logic.team_logic import Team_Logic
 
 
 class Logic_Wrapper:
     def __init__(self):
         self.data_wrapper = Data_Wrapper()
         self.player_logic = Player_logic(self.data_wrapper)
+        self.team_logic = Team_Logic(self.data_wrapper)
 
     def create_player(self, player):
         """Takes in customer object and forwards it to the data layer"""
@@ -16,3 +18,8 @@ class Logic_Wrapper:
         return self.player_logic.get_all_players()
 
         
+    def create_team(self):
+        return self.team_logic.create_team()
+
+    def get_captain(self):
+        return self.team_logic.get_captain()
