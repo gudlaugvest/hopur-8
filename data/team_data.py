e639fe359ed6c892ac4aa96768502a5da1cdd634
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2022-12-01 14:22:07 +0000

    hi

--- data/team_data.py
+++ data/team_data.py
@@ -7,11 +7,12 @@ class Team_Data:
         self.file_name = "file/team.csv"
 
     def create_team(self, team: Team):
+        i = len(self.get_all_teams()) + 1
         with open(self.file_name, "a", newline="", encoding="utf-8") as csvfile:
             fieldnames = ["id", "name", "captain_id", "club", "players_id"] 
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            writer.writerow({"id": team.id, "name": team.name, "captain_id": team.captain_id, "club": team.club, "players_id": team.players_id})
+            writer.writerow({"id": i, "name": team.name, "captain_id": team.captain_id, "club": team.club, "players_id": team.players_id})
 
     def get_all_teams(self):
         team_list = []
