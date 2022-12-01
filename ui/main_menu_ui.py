8a626204e9b0f66c0751fc2bb3665b35b5adbe26
astaduna <112621688+astaduna@users.noreply.github.com>
2022-12-01 12:36:43 +0000

    Update main_menu_ui.py

--- ui/main_menu_ui.py
+++ ui/main_menu_ui.py
@@ -1,4 +1,5 @@
 from logic.logic_wrapper import Logic_Wrapper
+from ui.captain_ui import Captain_UI
 from ui.general_user_ui import General_User_UI
 from ui.organizer_menu_ui import Organizer_Menu_UI
 
@@ -37,7 +38,8 @@ class MainMenu_UI:
                 general_user = General_User_UI(self.logic_wrapper)
                 general_user.input_prompt()
             elif command == "3":
-                pass
+                captain_menu = Captain_UI(self.logic_wrapper)
+                captain_menu.input_prompt()
             else:
                 ("Invalid input, try again!")
 
