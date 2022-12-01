b8caf0fbb416cb4b6f82d5a6a51634296ec3bf75
joejoe02 <johannagustha@gmail.com>
2022-12-01 11:34:59 +0000

    lll

---
+++ data/match_data.py
@@ -0,0 +1,10 @@
+class Match_Data:
+
+    def __init__(self, dataconnection):
+        pass
+
+    def create_match(self, match)
+        pass
+
+    def get_all_matches(self):
+        pass
---
+++ logic/match_logic.py
@@ -0,0 +1,12 @@
+class Match_Logic:
+
+
+    def __init__(self):
+
+        pass
+
+    def create_match(self):
+        pass
+
+    def list_all_matches(self):
+        pass
