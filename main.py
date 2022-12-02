f5156dc3271182dcad7e292f2fccbe53ff5d6f0c
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2022-12-02 09:43:31 +0000

    hello

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from ui.main_menu_ui import MainMenu_UI
-from ui.tournament_organizer_ui import Tournament_Organizer_UI
 from ui.tournament_ui import Tournament_UI
 from ui.club_ui import Club_UI
 
@@ -7,12 +6,9 @@ from ui.club_ui import Club_UI
 mainmenu = MainMenu_UI()
 mainmenu.input_prompt()
 
-organizermenu = Tournament_Organizer_UI()
-organizermenu.input_prompt()
 
 tournamentmenu = Tournament_UI()
 tournamentmenu.input_prompt()
 
 clubmenu = Club_UI()
 clubmenu.input_prompt()
-
