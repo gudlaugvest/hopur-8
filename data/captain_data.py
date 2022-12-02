30cc45159e8bed2110c09bbe01eca39ffda1c44d
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2022-12-02 11:03:32 +0000

    pu

--- data/captain_data.py
+++ data/captain_data.py
@@ -7,17 +7,17 @@ class Captain_data:
         self.file_name = "file/captain.csv"
 
     def create_team(self, captain: Captain):
-        i = len(self.get_all_teams()) + 1
+        #i = len(self.get_all_teams()) + 1
         with open(self.file_name, "a", newline="", encoding="utf-8") as csvfile:
-            fieldnames = ["name", "id"] 
+            fieldnames = ["id_number"] 
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            writer.writerow({"name": captain.name, "id": i})
+            writer.writerow({"id_number": captain.id_number})
 
     def get_all_teams(self):
         captain_list = []
         with open(self.file_name, newline="" ,encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                captain_list.append(Captain(row["name"], row["id"]))
+                captain_list.append(Captain(row["id"]))
         return captain_list
