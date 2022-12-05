50140ea73a84ba47439318a6950d44cf49539e8e
joejoe02 <johannagustha@gmail.com>
2022-12-05 11:50:06 +0000

    Update match_model.py

--- model/match_model.py
+++ model/match_model.py
@@ -1,9 +1,8 @@
 class Match:
-    def __init__(self, home_team, home_team_score, away_team, away_team_score):
+    def __init__(self, home_team, away_team):
         self.home_team = home_team
         self.away_team = away_team
-        self.home_team_score = home_team_score
-        self.away_team_score = away_team_score
+        
 
     def __str__(self):
-        return f"Home team: {self.home_team} {self.home_team_score}, Away team: {self.away_team} {self.away_team_score}"
+        return f"Home team: {self.home_team}, Away team: {self.away_team}"
