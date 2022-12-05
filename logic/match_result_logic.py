a1f485934a454fc8dc1478fdf0ae0e6d5312fcea
joejoe02 <johannagustha@gmail.com>
2022-12-05 15:06:43 +0000

    lll

--- logic/match_result_logic.py
+++ logic/match_result_logic.py
@@ -1,49 +1,42 @@
-
-from data.match_result_data import Match_Result_Data
-<<<<<<< HEAD
-
-=======
->>>>>>> 75900705f1cc5eb5e32ea98007c1314bda13c121
-
 class Match_Result_Logic:
 
-    def __init__(self):
-        self.match_result_data = Match_Result_Data()
+    def __init__(self, data_connections):
+        self.data_wrapper = data_connections
 
     def get_all_match_results(self):
-        return self.match_result_data.get_all_match_results()
+        return self.data_wrapper.get_all_match_results()
 
     def get_all_matches(self):
-        return self.match_result_data.get_all_match_results()
+        return self.data_wrapper.get_all_match_results()
 
     def unplayed_matches(self, match_list, date):
-        return self.match_result_data.unplayed_matches(match_list, date)
+        return self.data_wrapper.unplayed_matches(match_list, date)
         
     def played_matches(self, match_list, date):
-        return self.match_result_data.played_matches(match_list, date)
+        return self.data_wrapper.played_matches(match_list, date)
     
     
     def get_match_results(self, date):
-        return self.match_result_data.get_match_results(date)
+        return self.data_wrapper.get_match_results(date)
                 
 
     def get_unplayed_match_results(self, date): 
-        return self.match_result_data.get_unplayed_match_results(date)
+        return self.data_wrapper.get_unplayed_match_results(date)
         
 
     def get_match_result(self, home_team, type_of_game, away_team):
-        return self.match_result_data.get_match_result(home_team, type_of_game, away_team)
+        return self.data_wrapper.get_match_result(home_team, type_of_game, away_team)
         
     def create_match_result(self, home_team, type_of_game, away_team, results):
-        return self.match_result_data.create_match_result(home_team, type_of_game, away_team, results)
+        return self.data_wrapper.create_match_result(home_team, type_of_game, away_team, results)
         
 
     def new_method(self, home_team, type_of_game, away_team, results):
-        return self.match_result_data.new_method(home_team, type_of_game, away_team, results)
+        return self.data_wrapper.new_method(home_team, type_of_game, away_team, results)
         
 
     def update_match_result(self, home_team, type_of_game, away_team, results):
-        return self.match_result_data.update_match_result(home_team, type_of_game, away_team, results)
+        return self.data_wrapper.update_match_result(home_team, type_of_game, away_team, results)
 
     def delete_match_result(self, home_team, type_of_game, away_team):
-        return self.match_result_data.delete_match_result(home_team, type_of_game, away_team)   
+        return self.data_wrapper.delete_match_result(home_team, type_of_game, away_team)   
