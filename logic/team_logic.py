c437782da69f8070fb5ba2a549501a1b9a7e1910
joejoe02 <johannagustha@gmail.com>
2022-12-06 15:14:45 +0000

    lll

--- logic/team_logic.py
+++ logic/team_logic.py
@@ -11,19 +11,7 @@ class Team_Logic:
 
 
     def get_captain(self, team_id):
-        teams = self.get_all_teams()
-        correct_team = None
-        for team in teams:
-            if team.id == team_id:
-                correct_team = team
-                break
-        players = self.data_wrapper.get_all_players()
-        correct_player = None
-        for player in players:
-            if player.id == correct_team.team_captain:
-                correct_player = player
-                return correct_player
-        return None
+        return self.data_wrapper.get_captain(team_id)
 
 
     def get_all_teams(self):
@@ -31,7 +19,4 @@ class Team_Logic:
 
 
     def get_team_by_name(self, name):
-        all_teams = self.get_all_teams()
-        for team in all_teams:
-            if team.name == name:
-                return team
+        return self.data_wrapper.get_team_by_name(name)
