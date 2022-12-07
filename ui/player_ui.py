cb1b5459ea200989a6f4d36b66ebae154cf39a29
lotta <lotta22@ru.is>
2022-12-07 10:36:07 +0000

    hi

--- ui/player_ui.py
+++ ui/player_ui.py
@@ -33,26 +33,39 @@ class Player_UI:
                 for i in range(how_many):
                     print()
                     player = Player()
-                    player.name = input("Enter Name: ")
-                    player.ss_number = input("Enter Social Security Number(000000-0000): ")
-                    ss_number = map(int, ss_number.split("-"))
-                    while len(ss_number) != 2:
-                        print()
-                        print("Invalid Social Security Number!, Please Try Again!\n")
-                        player.ss_number = input("Enter Social Security Number: ")
-                    player.home_address = input("Enter Home Address: ")
-                    player.phone_number = input("Enter Phone Number: ")
-                    player.email_address = input("Enter Email Address: ")
-                    player.role = input("Enter Players Role(Captain/Player): ")
-                    team_name = input("Enter Team Name: ")
-                    team = self.logic_wrapper.get_team_by_name(team_name) 
-                    print()          
-                    
-                    if team is None:
-                        print("No team found with that name")
-                        return
-                    player.team_name = team.name
-                    self.logic_wrapper.create_player(player)
+                    while True:
+
+                        player.name = input("Enter Name: ")
+                        player.ss_number = input("Enter Social Security Number(000000-0000): ")
+
+                        try:
+                            if len(player.ss_number) != 12:
+                                return player.ss_number
+                        except:
+                            print("Invalid ss number, again")    
+                            
+                
+                            
+            
+                    #ss_number = map(int, ss_number.split("-"))
+                    #while len(ss_number) != 2:
+                        #print()
+                        #print("Invalid Social Security Number!, Please Try Again!\n")
+                        #player.ss_number = input("Enter Social Security Number: ")
+                        player.home_address = input("Enter Home Address: ")
+                        player.phone_number = input("Enter Phone Number: ")
+                        player.email_address = input("Enter Email Address: ")
+                        player.role = input("Enter Players Role(Captain/Player): ")
+                        team_name = input("Enter Team Name: ")
+                        team_name = team_name.lower()
+                        team = self.logic_wrapper.get_team_by_name(team_name) 
+                        print()          
+                        
+                        if team is None:
+                            print("No team found with that name")
+                            return
+                        player.team_name = team.name
+                        self.logic_wrapper.create_player(player)
 
         else:
             print()
