1a3691f2734bce1ea3973ae991505f26155d31c6
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2022-12-07 13:41:19 +0000

    hi

--- ui/tournament_ui.py
+++ ui/tournament_ui.py
@@ -41,25 +41,4 @@ class Tournament_UI:
             year, month, day = [int(item) for item in tournament.end_date]
             tournament.end_date = datetime(year, month, day)
             self.logic_wrapper.create_tournament(tournament)
-
-
-"""
-
-year = 2021
-month = 12
-day = 26
-date = datetime(year, month, day)
-date_search_from = datetime(2021, 12, 10)
-date_search_to = datetime(2021, 12, 31)
-if date_search_from <= date <= date_search_to:
-    print("date inbetween")
-else:
-    print("date out of range")
-date_search_to = datetime(2021, 12, 1)
-if date_search_from <= date <= date_search_to:
-    print("date inbetween")
-else:
-    print("date out of range")
-        
-"""
             
