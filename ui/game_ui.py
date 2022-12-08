4b5480ae796bf5fa1554c1e43a930092090a217c
joejoe02 <johannagustha@gmail.com>
2022-12-08 14:22:43 +0000

    Update game_ui.py

--- ui/game_ui.py
+++ ui/game_ui.py
@@ -15,4 +15,12 @@ class Game_UI:
     def input_prompt(self):
         while True:
             self.menu_output()
-            home_team_player_ssn = input("Enter Home Team Player SSN: ")
+            game_counter = 0
+            while game_counter != 4:
+                home_team_player_ssn = input("Enter Home Team Player SSN: ")
+                away_team_players_ssn = input("Enter Away Team Player SSN: ")
+                score = input("Enter Score: ")
+                type_of_game = input("Enter Type of Game: ")
+                game = Game_Model(home_team_player_ssn, away_team_players_ssn, score, type_of_game)
+                game_counter += 1
+                self.logic_wrapper.create_game(game)
