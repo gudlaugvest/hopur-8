0e00bc0750e3f8b658cbfbcb0d7914faf31523d0
lotta <lotta22@ru.is>
2022-12-08 21:15:17 +0000

    hi

--- ui/tournament_ui.py
+++ ui/tournament_ui.py
@@ -18,18 +18,52 @@ class Tournament_UI:
 
 
     def input_prompt(self):
+        while True:
+            self.menu_output()
+            print()
+            command = input("Enter Command: ")
+            command = command.lower()
+            if command == "b":
+                break
+            elif command == "q":
+                return "q"
+            elif command == "1":
+                self.create_tournament_menu()
+            elif command == "2":
+                self.update_tournament_menu()
+            else:
+                print()
+                print("Invalid input, please try again!")    
     
 
 
-    def create_tournament():
-        print()
+    def create_tournament_menu():
+        pass
+        '''print()
         print("####################################")
         print()
         print("Create Tournament".rjust(23))
         print()
 
+        tournament = Tournament()
+        tournament.name = input("Enter Tournament name: ")
+        tournament.start_date = input("Enter start date(yyyy.mm.dd): ").split(".")
+        while len(tournament.start_date) != 3:
+            print()
+            print("Date format is incorrect! Please Try again!")
+            tournament.start_date = input("Enter start date(yyyy.mm.dd): ")
+        year, month, day =  [int(item) for item in tournament.start_date]
+        tournament.start_date = datetime(year, month, day)
+        tournament.end_date = input("Enter end date(yyyy.mm.dd): ").split(".")
+        while len(tournament.end_date) != 3:
+            print("Date format is incorrect! Please Try again!")
+            tournament.end_date = input("Enter end date(yyyy.mm.dd): ").split(".")
+        year, month, day = [int(item) for item in tournament.end_date]
+        tournament.end_date = datetime(year, month, day)
+        self.logic_wrapper.create_tournament(tournament)'''
 
-    def update_tournament():
+
+    def update_tournament_menu():
         print()
         print("####################################")
         print()
@@ -56,24 +90,8 @@ class Tournament_UI:
         
         
         
-       ''' 
-        while True:
-            self.menu_output()
-            print()
-            command = input("Enter Command: ")
-            command = command.lower()
-            if command == "b":
-                break
-            elif command == "q":
-                return "q"
-            elif command == "1":
-                self.create_match_menu()
-            elif command == "2":
-                self.update_match_menu()
-            else:
-                print()
-                print("Invalid input, please try again!")    
-        self.menu_output()
+       
+        '''self.menu_output()
         print()
         command = input("Enter Command: ")
         print()
