b732b7a50f03e29233c496eb847972ce8103521d
joejoe02 <johannagustha@gmail.com>
2022-12-08 14:23:24 +0000

    Update game_model.py

--- model/game_model.py
+++ model/game_model.py
@@ -1,9 +1,9 @@
 
 class Game_Model:
 
-    def __init__(self, home_team_player_ssn="", away_team_players_ssn="", score="", type_of_game=""):
+    def __init__(self, home_team_player_ssn="", away_team_player_ssn="", score="", type_of_game=""):
         self.home_team_player_ssn = home_team_player_ssn
-        self.away_team_players_ssn = away_team_players_ssn
+        self.away_team_player_ssn = away_team_player_ssn
         self.score = score
         self.type_of_game = type_of_game
 
