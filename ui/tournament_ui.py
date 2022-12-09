04eb71102ef5cb948f514f329abfd6c88c6a30c2
lotta <lotta22@ru.is>
2022-12-09 11:41:36 +0000

    hi

--- ui/tournament_ui.py
+++ ui/tournament_ui.py
@@ -18,31 +18,34 @@ class Tournament_UI:
 
 
     def input_prompt(self):
-        self.menu_output()
-        print()
-        command = input("Enter Command: ")
-        print()
-        if command == "b":
-            return "b"
-        elif command == "1":
-            tournament = Tournament()
-            tournament.name = input("Enter Tournament name: ")
-            tournament.start_date = input("Enter start date(yyyy.mm.dd): ").split(".")
-            while len(tournament.start_date) != 3:
-                print()
-                print("Date format is incorrect! Please Try again!")
-                tournament.start_date = input("Enter start date(yyyy.mm.dd): ")
-            year, month, day =  [int(item) for item in tournament.start_date]
-            tournament.start_date = datetime(year, month, day)
-            tournament.end_date = input("Enter end date(yyyy.mm.dd): ").split(".")
-            while len(tournament.end_date) != 3:
-                print("Date format is incorrect! Please Try again!")
+        while True:
+            self.menu_output()
+            print()
+            command = input("Enter Command: ")
+            print()
+            if command == "b":
+                return "b"
+            elif command == "1":
+                tournament = Tournament()
+                tournament.name = input("Enter Tournament name: ")
+                tournament.start_date = input("Enter start date(yyyy.mm.dd): ").split(".")
+                while len(tournament.start_date) != 3:
+                    print()
+                    print("Date format is incorrect! Please Try again!")
+                    tournament.start_date = input("Enter start date(yyyy.mm.dd): ")
+                year, month, day =  [int(item) for item in tournament.start_date]
+                tournament.start_date = datetime(year, month, day)
                 tournament.end_date = input("Enter end date(yyyy.mm.dd): ").split(".")
-            year, month, day = [int(item) for item in tournament.end_date]
-            tournament.end_date = datetime(year, month, day)
-            self.logic_wrapper.create_tournament(tournament)
-        elif command == "2":
-            pass
+                while len(tournament.end_date) != 3:
+                    print("Date format is incorrect! Please Try again!")
+                    tournament.end_date = input("Enter end date(yyyy.mm.dd): ").split(".")
+                year, month, day = [int(item) for item in tournament.end_date]
+                tournament.end_date = datetime(year, month, day)
+                self.logic_wrapper.create_tournament(tournament)
+            elif command == "2":
+                pass
+            else:
+                print("Invalid input, please try again!")
             
             '''update_tournamnet = self.logic_wrapper.update_tournament(tournament)
             for info in update_tournamnet:
