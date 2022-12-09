ca0160335a0d6b27ac7630de819e9e6555aa145e
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2022-12-09 14:55:15 +0000

    hi

--- data/tournament_data.py
+++ data/tournament_data.py
@@ -31,29 +31,3 @@ class Tournament_Data:
                 #ending_date = datetime(year=end_date.split('.')[2], month=end_date.split('.')[1], day=end_date.split('.')[0])
                 tournament_list.append(Tournament(row["id"], row["name"],row["start_date"], row["end_date"]))
         return tournament_list
-
-    def update_tournament(self, tournament):
-        """Update Tournament"""
-        tournament_list = self.get_all_tournaments()
-        for i in range(len(tournament_list)):
-            if tournament.id == tournament_list[i].id:
-                tournament_list[i] = tournament
-        
-
-        with open(self.file_name, "w", newline="", encoding="utf-8") as csvfile:
-            fieldnames = ["id", "name", "start_date", "end_date"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for i in range(len(tournament_list)):
-                writer.writerow({"id": tournament_list[i].id, "name": tournament_list[i].name, "start_date": tournament_list[i].start_date, "end_date": tournament_list[i].end_date})
-                
-        
-        
-               
-
-    def register_type_of_match(self):
-        '''This function specifies what type of match'''
-        pass
-
-    def get_all_games_in_tournament(self, tournament):
-        pass
