96cf5265cab3cdf396f91d5a6460dece3d6093a3
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2022-12-09 12:29:50 +0000

    hi

--- ui/player_ui.py
+++ ui/player_ui.py
@@ -19,84 +19,82 @@ class Player_UI:
     def input_prompt(self):
         '''The function counts how many players the Organizer wants to input and inputs the information 
         for each players and gives role for 1 captain and rest players'''
-        while True:
-            self.menu_output()
+        self.menu_output()
+        print()
+        command = input("Enter Command: ")
+        if command == "b":
+            return "b"
+        elif command == "c":
             print()
-            command = input("Enter Command: ")
-            if command == "b":
-                return "b"
-            elif command == "c":
-                print()
-                #villucheck
-                while True:
-                    try:
-                        how_many = int(input("How many players?: "))
+            #villucheck
+            while True:
+                try:
+                    how_many = int(input("How many players?: "))
+                    if how_many < 4:
+                        return
 
-                        if how_many >= 4:
-                            break    
-
-                        else:
-                            print()
-                            print("Players for each team has to be at least 4! Please try again!")
-                    except:
-                        ValueError
-                        for i in range(how_many):
-                            print()
-                            player = Player()
-                            player.name = input("Enter Player name: ")
-                            while True:
-                                try:
-                                    player.ss_number = input("Enter Social Security Number(0000000000): ")
-                                    if len(player.ss_number) == 10:
-                                        break
-                                    else:
-                                        print()
-                                        print("Invalid ss number, try again")
-                                except:
-                                    ValueError
+                    else:
+                        print()
+                        print("Players for each team has to be at least 4! Please try again!")
+                except:
+                    ValueError
+                    for i in range(how_many):
+                        print()
+                        player = Player()
+                        player.name = input("Enter Player name: ")
+                        while True:
+                            try:
+                                player.ss_number = input("Enter Social Security Number(0000000000): ")
+                                if len(player.ss_number) == 10:
+                                    break
+                                else:
+                                    print()
+                                    print("Invalid ss number, try again")
+                            except:
+                                ValueError
+                            
+                        player.home_address = input("Enter Home Address: ")
+                        while True:
+                            try:
+                                player.phone_number = input("Enter phone number: ")
+                                if len(player.phone_number) == 7:
+                                    break
+                                else:
+                                    print()
+                                    print("Not a valid phone number, try againn")
+                                    
+                            except:
+                                ValueError
+                        player.email_address = input("Enter Email Address: ")
+                        while True:    
+                            try:
+                                player.role = input("Enter Players Role(Captain/Player): ")
+                                if player.role == "Player".lower():
+                                    break
+                                elif player.role =="Captain".lower():
+                                    break
+                                else:
+                                    print()
+                                    print("Not a valid role, try again")
+                            except:
+                                ValueError
                                 
-                            player.home_address = input("Enter Home Address: ")
-                            while True:
-                                try:
-                                    player.phone_number = input("Enter Phone Number: ")
-                                    if len(player.phone_number) == 7:
-                                        break
-                                    else:
-                                        print()
-                                        print("Not a valid phone number, try againn")
-                                        
-                                except:
-                                    ValueError
-                            player.email_address = input("Enter Email Address: ")
-                            while True:    
-                                try:
-                                    player.role = input("Enter Players Role(Captain/Player): ")
-                                    if player.role == "Player".lower():
-                                        break
-                                    elif player.role =="Captain".lower():
-                                        break
-                                    else:
-                                        print()
-                                        print("Not a valid role, try again")
-                                except:
-                                    ValueError
-                                    
-                                    
+                                
+                        team_name = input("Enter Team Name: ")
+                        team_name = team_name.lower()
+                        team = self.logic_wrapper.get_team_by_name(team_name) 
+                        while team is None:
+                            print("No team found with that name")
                             team_name = input("Enter Team Name: ")
                             team_name = team_name.lower()
-                            team = self.logic_wrapper.get_team_by_name(team_name) 
-                            while team is None:
-                                print("No team found with that name")
-                                team_name = input("Enter Team Name: ")
-                                team_name = team_name.lower()
-                                team = self.logic_wrapper.get_team_by_name(team_name)  
+                            team = self.logic_wrapper.get_team_by_name(team_name)  
 
 
-                        print()          
-                        player.team_id = team.id
-                        self.logic_wrapper.create_player(player)   
+                    print()          
+                    player.team_id = team.id
+                    self.logic_wrapper.create_player(player)   
      
-            else:
-                print()
-                print("Invalid input, please try again!")
+        else:
+            print()
+            print("Invalid input, please try again!")
 
